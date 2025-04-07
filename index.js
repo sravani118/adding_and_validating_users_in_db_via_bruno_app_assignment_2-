25da79430a669ede60bb74453a757fecef9b0b69
penumajji sravani <penumajjisravani698@gmail.com>
2025-04-07 17:29:36 +0530

    login completed

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
+require('dotenv').config();
 const express = require('express');
-const { resolve } = require('path');
-
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const User = require('./models/User');
 const app = express();
-const port = 3010;
+app.use(express.json()); 
 
-app.use(express.static('static'));
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected sucessfully'))
+.catch(err => console.error('MongoDB connection failed', err));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ error: 'Email and password are required' });
+    }
+
+    try {
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(404).json({ error: 'User already exists' });
+        }
+
+        const hashedPasword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({email, password: hashedPasword})
+        await newUser.save();
+
+        res.status(200).json({ message: 'Login successful', user:newUser });
+    } catch (error) {
+        res.status(500).json({ error: 'Server error' });
+    }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
 });
